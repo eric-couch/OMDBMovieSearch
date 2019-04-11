bf5ebc11abf47efb109f66c5b962a316a40d062d
eric-couch <James.Couch@edgetechacademy.edu>
2019-04-10 22:30:47 -0500

    add favorites

--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 let fetch = require("node-fetch");
 
+let favMovies = [];
+
 router.post("/getMovie", function(req, res, next) {
   let movieTitle = req.body.movieTitle;
   let fetchURL = "https://www.omdbapi.com/?apikey=86c39163&t=" + movieTitle;
@@ -20,7 +22,7 @@ router.get("/getMovie", function(req, res, next) {
     .then(body => res.render("movies", { movie: body }));
 });
 
-router.post("/searchMovies", function(req, res, next) {
+router.post("/", function(req, res, next) {
   let movieTitle = req.body.movieTitle;
   let movieYear = req.body.movieYear;
   let movieType = req.body.movieType;
@@ -52,4 +54,20 @@ router.post("/searchMovies", function(req, res, next) {
     );
 });
 
+router.post("/addFav", function(req, res, next) {
+  // a post of some imdb # comes into this function
+  // req.body.imdbID
+  // fetch('https://www.omdbapi.com/?apikey=86c39163&i=' + req.body.imdbID)
+  // favMovies.push(res.body)
+  let i = req.body.imdb;
+  let fetchURL = "https://www.omdbapi.com/?apikey=86c39163&i=" + i;
+  fetch(fetchURL)
+    .then(r => r.json())
+    .then(body => favMovies.push(body));
+  res.send("Movie Added!");
+});
+
+router.get("/getFavs", function(req, res, next) {
+  res.send(favMovies);
+});
 module.exports = router;
