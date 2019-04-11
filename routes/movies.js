51534c9f113d35aec68ee9ed49bfe1a2610c1d0f
eric-couch <James.Couch@edgetechacademy.edu>
2019-04-10 20:44:06 -0500

    added year and type

--- routes/movies.js
+++ routes/movies.js
@@ -22,30 +22,34 @@ router.get("/getMovie", function(req, res, next) {
 
 router.post("/searchMovies", function(req, res, next) {
   let movieTitle = req.body.movieTitle;
+  let movieYear = req.body.movieYear;
+  let movieType = req.body.movieType;
   let nextPage = req.body.page;
-  if (nextPage) {
-    let fetchURL =
-      "https://www.omdbapi.com/?apikey=86c39163&s=" +
-      movieTitle +
-      "&page=" +
-      req.body.page;
-    fetch(fetchURL)
-      .then(r => r.json())
-      .then(body =>
-        res.render("moviesList", {
-          search: movieTitle,
-          page: nextPage++,
-          movies: body
-        })
-      );
-  } else {
-    let fetchURL = "https://www.omdbapi.com/?apikey=86c39163&s=" + movieTitle;
-    fetch(fetchURL)
-      .then(r => r.json())
-      .then(body =>
-        res.render("moviesList", { search: movieTitle, page: 2, movies: body })
-      );
+  let fetchURL = "https://www.omdbapi.com/?apikey=86c39163&s=" + movieTitle;
+  if (movieYear) {
+    fetchURL += "&y=" + movieYear;
   }
+  if (movieType) {
+    if (movieType != "All") {
+      fetchURL += "&type=" + movieType;
+    }
+  }
+  if (nextPage) {
+    fetchURL += "&page=" + nextPage;
+    nextPage++;
+  } else {
+    nextPage = 2;
+  }
+
+  fetch(fetchURL)
+    .then(r => r.json())
+    .then(body =>
+      res.render("moviesList", {
+        search: movieTitle,
+        page: nextPage,
+        movies: body
+      })
+    );
 });
 
 module.exports = router;
